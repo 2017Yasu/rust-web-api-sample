82371f67d35713d24f454ba1e1e17a0498189c68
2017Yasu <yasu.nkym.96@gmail.com>
2023-04-08 23:33:35 +0900

    create simple api

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,69 @@
+use std::sync::Mutex;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+struct AppState {
+    app_name: String,    // shared state
+    counter: Mutex<i32>, // shared mutable state
 }
 
+// /
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    HttpResponse::Ok().body(format!("Welcome to {app_name}!"))
+}
+
+// /api/count
+#[get("/count")]
+async fn count(data: web::Data<AppState>) -> impl Responder {
+    let counter = data.counter.lock().unwrap();
+    HttpResponse::Ok().body(format!("Request number: {counter}"))
+}
+
+// /echo
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+// /api
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        // /api/test
+        web::resource("/test")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    )
+    .service(count);
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        // /app
+        web::resource("/app")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let state = web::Data::new(AppState {
+        app_name: String::from("web-api-sample"),
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
+            .app_data(state.clone())
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .service(index)
             .service(echo)
-            .route("hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
